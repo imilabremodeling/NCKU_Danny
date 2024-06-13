1a8505230a3d1d3b7221907556f87adbc04f96de
slchenncku12 <slchenncku12@gmail.com>
2024-06-14 04:13:00 +0800

    04:13:00

--- predict.py
+++ predict.py
@@ -59,7 +59,7 @@ RF2 = load(str(joblib_files[0]))
 
 
 #設定OPCUA伺服器位置
-server_url = 'opc.tcp://192.168.15.80:4840/freeopcua/server/' 
+server_url = 'opc.tcp://192.168.15.35:4840/freeopcua/server/' 
 #與伺服器建立OPCUA連結
 client = Client(server_url)
 
@@ -89,7 +89,7 @@ try:
             Product_ID = temp_var.get_value()
             print("Current Type:", Product_ID)
             Product_ID_ar.append(Product_ID)
-            feedback+=("CnCAbnormalDetect&cnc&"+str(Product_ID)+"&")
+            feedback+=("CnCAbnormalDetect2&cnc2&"+str(Product_ID)+"&")
 
 
             # 假设我们知道变量的节点ID
